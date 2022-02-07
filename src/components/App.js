c05182047f83f8f5f133d45d3fd3056e42f51944
elenavedaleva <elenavedaleva@gmail.com>
2022-02-07 11:20:02 -0500

    add search bar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import SearchBar from './SearchBar';
+import youtube from '../api/youtube';
 
 class App extends React.Component {
+  state = { videos: [] };
+
+  onTermSubmit = async (term) => {
+    const response = await youtube.get('/search', {
+      params: {
+        q: term
+      }
+    });
+
+    console.log(response);
+    this.setState({ videos: response.data.items})
+  };
+
   render() {
-    return <div>App</div>;
+    return (
+      <div className="ui container" style={{ marginTop: '10px'}}>
+        <SearchBar onTermSubmit={this.onTermSubmit} />
+        I have {this.state.videos.length} videos
+      </div>
+    );
   }
 }
 
